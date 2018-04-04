980219d57bf989e59e3a6f78d0bec16f0e357892
Nancy Vidales <nancvidales@gmail.com>
2018-04-04 01:04:29 -0500

    added all view controller files to be used, created alarm model and alarm table view cell files, added main view controller to storyboard, modified launch screen storyboard

---
+++ ElevateYourMorning/Model/Alarm.swift
@@ -0,0 +1,16 @@
+//
+//  Alarm.swift
+//  ElevateYourMorning
+//
+//  Created by Nancy Vidales on 4/4/18.
+//  Copyright © 2018 Nancy Vidales. All rights reserved.
+//
+
+import Foundation
+
+class Alarm{
+    //date
+    //label
+    //SMTWTFS buttons
+    //SMTWTFS buttons tapped
+}
---
+++ ElevateYourMorning/View/AlarmTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmTableViewCell.swift
+//  ElevateYourMorning
+//
+//  Created by Nancy Vidales on 4/4/18.
+//  Copyright © 2018 Nancy Vidales. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
